d690bace8732a7ccf064924e57bcebbc65991556
Rummel17 <p.rummel17@gmail.com>
2018-03-13 21:05:23 +0100

    test

--- resources/js/main.js
+++ resources/js/main.js
@@ -4,5 +4,8 @@ slidesToShow: 1,
 slidesToScroll: 1,
 autoplay: true,
 autoplaySpeed: 4000,
+draggable:false,//disables draggable per mouse
+nextArrow:false,
+prevArrow:false,//disables arrows and closes gap to header, cannot be disabled with css
 });
 });
